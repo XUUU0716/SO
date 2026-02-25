ca75bbd7706e55c9b8f7fcd939bb566cddb764f8
xushaofan <xushaofan716@gmail.com>
2026-02-25 17:27:01 +0100

    changes

--- practica1/miner.c
+++ practica1/miner.c
@@ -5,31 +5,35 @@
 #include <unistd.h>
 #include <pthread.h>
 #include "pow.h"
+#include <string.h>
 
 #define ROUNDS 5
 #define N_THREADS 8
 
 int globalSolution = 0;
 int findSolution = 0;
-
+int globalTarget=0;
 typedef struct Thread_args
 {
     long int start;
     long int end;
-    long int target;
+    //long int target;
 
 }Thread_args;
 
-void *miner(Thread_args *t)
+void *miner(void *arg)
 {
+    Thread_args *t=(Thread_args*)arg;
     for (long int i = t->start; i < t->end && !findSolution; i++)
     {
-        if (pow_hash(i) == t->target)
+        if (pow_hash(i) == globalTarget)
         {
             findSolution = 1;
             globalSolution = i;
+            return NULL; 
         }
     }
+    return NULL;
 }
 
 /*error = pthread_create(&h2, NULL, slow_printf, world);
@@ -38,33 +42,69 @@ void *miner(Thread_args *t)
     exit(EXIT_FAILURE);
   }*/
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 
-    pthread_t *h;
-    Thread_args t[N_THREADS];
+    pthread_t *threads;
+    Thread_args *args;
+    int rounds;
+    int nThreads;
+    int targetIni;
+
     if (argc < 4)
     {
         fprintf(stderr, "Usage ./miner < TARGET_INI > < ROUNDS > < N_THREADS >\n");
-    }
-    if ((h = malloc(sizeof(pthread_t) * N_THREADS)) == NULL)
-    {
-        fprintf(stderr, "malloc\n");
         exit(EXIT_FAILURE);
     }
-    for (int j = 0; j < ROUNDS; j++)
+
+    // Read arguments 
+    targetIni=atoi(argv[1]);
+    rounds=atoi(argv[2]);
+    nThreads=atoi(argv[3]);
+
+    // Create n_threads threads_args
+    if((args=malloc(sizeof(Thread_args)*nThreads))==NULL)
     {
-        for (int i = 0; i < N_THREADS; i++)
-        {
-            int error = pthread_create(&h[i], NULL, miner,&t[i]);
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);      
+    }
+
+    if ((threads = malloc(sizeof(pthread_t) * nThreads)) == NULL)
+    {
+        free(args);
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    //Asignation of target init
+    globalTarget=targetIni;
+
+    for (long long j = 0; j < rounds; ++j)
+    {
+        for (long long i = 0; i < nThreads; ++i)
+        {   
+            args[i].start=i*(POW_LIMIT)/nThreads;
+            args[i].end=(i+1)*(POW_LIMIT)/nThreads;
+
+            int error = pthread_create(&threads[i], NULL, miner, &args[i]);
             if (error != 0)
             {
                 fprintf(stderr, "pthread_create: %s\n", strerror(error));
                 exit(EXIT_FAILURE);
             }
 
-            return 0;
         }
+        for(long long i=0;i<nThreads;++i)
+        {
+            pthread_join(threads[i],NULL);
+        }
+        fprintf(stdout,"Solution ronda %lld: %d\n",j,globalSolution);
 
+        //set new target 
+        findSolution=0;
+        globalTarget=globalSolution;
     }
+    free(args);
+    free(threads);
+    return 0;
 }
